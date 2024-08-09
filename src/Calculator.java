5f53e34ef13cfa37da3dcb9484d68b7191d7b963
MAnitsu <nitu.alexandru96@gmail.com>
2024-08-09 20:51:56 +0300

    improved calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,36 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        double x = 15.5;
-        int y = 5;
-        double sum = x + (double) y;
-        double diff = x - (double) y;
-        double prod = x * (double) y;
-        double quot = x / (double) y;
-        System.out.println("The sum of the numbers is: " + sum);
-        System.out.println("The difference of the numbers is: " + diff);
-        System.out.println("The product of the numbers is: " + prod);
-        System.out.println("The quotient of the numbers is: " + quot);
+        Scanner keyboard = new Scanner(System.in);
+
+        double firstNumber;
+        double secondNumber;
+        String operation;
+
+        System.out.print( "Insert first number: " );
+        firstNumber = keyboard.nextDouble();
+        System.out.print( "Insert second number: " );
+        secondNumber = keyboard.nextDouble();
+        System.out.print( "What operation do you want? (sum, difference, product, quotation) " );
+        operation = keyboard.next().toLowerCase();
+
+        if (operation.equals("sum")) {
+            double sum = firstNumber + secondNumber;
+            System.out.println("The sum of the numbers is: " + sum);
+        } else if (operation.equals("difference")) {
+            double diff = firstNumber - secondNumber;
+            System.out.println("The difference of the numbers is: " + diff);
+        } else if (operation.equals("product")) {
+            double prod = firstNumber * secondNumber;
+            System.out.println("The product of the numbers is: " + prod);
+        } else if (operation.equals("quotation")) {
+            double quot = firstNumber / (double) secondNumber;
+            System.out.println("The quotient of the numbers is: " + quot);
+        } else {
+            System.out.println("Invalid choice");
+        }
+
+
     }
 }
