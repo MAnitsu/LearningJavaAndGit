6e3652ad69bfa260889d467f12d01e2afcee5581
MAnitsu <nitu.alexandru96@gmail.com>
2024-08-20 17:16:07 +0300

    updated fibonacci.java

--- src/week2/fibonacci.java
+++ src/week2/fibonacci.java
@@ -1,16 +1,31 @@
 package week2;
 
 public class fibonacci {
+    private static long[] fibonacciCache;
+
     public static void main(String[] args) {
-        int i = 0;
-        int[] myArray = {0, 1};
-        while (i < 10) {
-            i++;
-            int sum = myArray[0] + myArray[1];
-            myArray[0] = sum;
-            myArray[1] += myArray[0];
-            System.out.println(myArray[0]);
-            System.out.println(myArray[1]);
+
+        int n = 10;
+
+        fibonacciCache = new long[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fib(i) + " ");
         }
+    }
+
+    private static long fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        if (fibonacciCache[n] != 0) {
+            return fibonacciCache[n];
+        }
+
+        long nthFibonacciNumber = (fib(n - 1) + fib(n - 2));
+        fibonacciCache[n] = nthFibonacciNumber;
+
+        return nthFibonacciNumber;
     }
 }
--- src/week2/multiplicationTable.java
+++ src/week2/multiplicationTable.java
@@ -4,7 +4,7 @@ public class multiplicationTable {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
             System.out.println();
-            for (int j = 0; j <= 10; j++){
+            for (int j = 0; j <= 10; j++) {
                 System.out.println(i + " * " + j + " = " + (i * j));
             }
         }
